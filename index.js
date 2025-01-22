42dd1426d9fec856c99b499faacfdfaf10dd9e6a
rel-dimotek <151809574+rel-dimotek@users.noreply.github.com>
2025-01-22 14:38:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('./public/index.html', {
+        root: __dirname
+    });
+});
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
